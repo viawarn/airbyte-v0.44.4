6b615626968412593ec46f232efbee8916a77f0f
kathy <guepaz@gmail.com>
2023-06-01 06:26:38 -0600

    passing unit tests and check-connection function

--- airbyte-integrations/connectors/source-viawarn-us-ak-dot/unit_tests/test_source.py
+++ airbyte-integrations/connectors/source-viawarn-us-ak-dot/unit_tests/test_source.py
@@ -6,11 +6,14 @@ from unittest.mock import MagicMock
 
 from source_viawarn_us_ak_dot.source import SourceViawarnUsAkDot
 
+import json
 
 def test_check_connection(mocker):
     source = SourceViawarnUsAkDot()
-    logger_mock, config_mock = MagicMock(), MagicMock()
-    assert source.check_connection(logger_mock, config_mock) == (True, None)
+    with open("secrets/config.json", "r") as f:
+        config = json.load(f)
+    logger_mock = MagicMock()
+    assert source.check_connection(logger_mock,config) == (True, None)
 
 
 def test_streams(mocker):
@@ -18,5 +21,6 @@ def test_streams(mocker):
     config_mock = MagicMock()
     streams = source.streams(config_mock)
     # TODO: replace this with your streams number
-    expected_streams_number = 2
+    expected_streams_number = 3
     assert len(streams) == expected_streams_number
+
